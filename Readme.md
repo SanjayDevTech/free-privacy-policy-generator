47a18130f3199b7a9cf5c0caadef572c11c7b07a
Sanjay Developer <49263351+Sanjay-Developer-Official@users.noreply.github.com>
2019-04-04 15:51:45 +0530

    Free Privacy Policy Generator

---
+++ Readme.md
@@ -0,0 +1,69 @@
+# App Privacy Policy Generator
+A simple web app to generate a generic privacy policy for your Android/iOS apps
+
+Check out the [web app](https://free-privacy-policy-generator.firebaseapp.com/)!
+
+### Featured in
+[![AndroidDev Digest](https://img.shields.io/badge/AndroidDev%20Digest-%23133-blue.svg)](https://www.androiddevdigest.com/digest-133/)
+
+### Show some :heart:
+[![GitHub stars](https://img.shields.io/github/stars/nisrulz/app-privacy-policy-generator.svg?style=social&label=Star)](https://github.com/Sanjay-Developer-Official/free-privacy-policy-generator) [![GitHub forks](https://img.shields.io/github/forks/nisrulz/app-privacy-policy-generator.svg?style=social&label=Fork)](https://github.com/Sanjay-Developer-Official/free-privacy-policy-generator/fork) [![GitHub watchers](https://img.shields.io/github/watchers/nisrulz/app-privacy-policy-generator.svg?style=social&label=Watch)](https://github.com/Sanjay-Developer-Official/free-privacy-policy-generator) [![GitHub followers](https://img.shields.io/github/followers/nisrulz.svg?style=social&label=Follow)](https://github.com/Sanjay-Developer-Official/free-privacy-policy-generator)  
+
+## Web App Screenshot
+![screenshot](/img/sc1.jpg)
+
+# Contributing more 3rd Party Service's Privacy links
+The webapp uses a JSON array to populate the list of 3rd party services section. If you want contribute a new 3rd party service for the section then open a Pull Request which simply adds a new JSON item to the JSON Array in [`public/js/thirdpartyservices.js`](public/js/thirdpartyservices.js)
+
+The format is very simple and you only need to provide fill the fields as follows:
+```
+[
+  ..
+  }, // Donot forget to add a ',' before adding your new item in the array
+  {
+    name: 'Google Play Services',
+    model: 'gps', // this is stores the name of the model
+    gps: true, // this stores the state of the model, notice the key is the same as the model above. It is required by code.
+    logo: 'images/gps.png',
+    link: {
+      privacy: 'https://www.google.com/policies/privacy/',
+      terms: ''
+    }
+  }
+]
+```
+> Note: You also will have to drop the logo image into the [`public/images/`](public/images/) folder. Make sure the logo is 160 × 160 in size.
+
+# Pull Requests
+I welcome and encourage all pull requests. It usually will take me within 24-48 hours to respond to any issue or request. Here are some basic rules to follow to ensure timely addition of your request:
+  1. If its a feature, bugfix, or anything please only change code to what you specify.
+  1. Please keep PR titles easy to read and descriptive of changes, this will make them easier to merge :)
+  1. Pull requests _must_ be made against `develop` branch. Any other branch (unless specified by the maintainers) will get rejected.
+  1. Check for existing [issues](https://github.com/Sanjay-Developer-Official/free-privacy-policy-generator/issues/) first, before filing an issue.
+  1. Have fun!
+
+## Author & Support
+This project was created by [Nishant Srivastava](https://github.com/Sanjay-Developer-Official/) but hopefully developed and maintained by many others. See the [the list of contributors here](https://github.com/nisrulz/app-privacy-policy-generator/graphs/contributors).
+
+> If you appreciate my work, consider buying me a cup of :coffee: to keep me recharged :metal:
+>  + [PayPal](https://www.paypal.me/azpay123/5usd)
+
+>
+> I love using my work and thus maintain and keep [my open source projects](https://github.com/Sanjay-Developer-Official/) up to date!
+
+License
+=======
+
+    Copyright 2017 Nishant Srivastava
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ public/js/main.js
@@ -0,0 +1,133 @@
+/*  Copyright 2017 Nishant Srivastava
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License. */
+
+var vueapp = new Vue({
+  el: '#vueapp',
+  data: {
+    iOrWe: '[I/We]',
+    typeOfApp: '',
+    typeOfAppTxt: '[open source/free/freemium/ad-supported/commercial]',
+    typeOfDev: '',
+    appName: '[App Name]',
+    myOrOur: '[my/our]',
+    meOrUs: '[me/us]',
+    atNoCost: '[at no cost]',
+    retainedInfo: '[retained on your device and is not collected by [me/us] in any way]/[retained by us and used as described in this privacy policy]',
+    devName: '',
+    companyName: '',
+    devOrCompanyName: '[Developer/Company name]',
+    pidInfoIn: '',
+    pidInfo: '[add whatever else you collect here, e.g. users name, address, location, pictures]',
+    gps: true,
+    admob: false,
+    firebase_analytics: false,
+    piwik: false,
+    fabric: false,
+    clicky: false,
+    crashlytics: false,
+    facebook: false,
+    osType: '',
+    requirementOfSystem: 'system',
+    thirdPartyServices: thirdPartyServicesJsonArray
+  },
+  methods: {
+    selectAllText: function (id) {
+      selectText(id)
+    },
+    toggleState: function (item) {
+      let state = item.model
+
+      console.log('Item:', item.name, item.model, item[state])
+      // For reactive update of the json
+      // Toggle the state
+      Vue.set(thirdPartyServicesJsonArray, item.model, !item[state])
+    },
+    generateHTML: function (id, filename) {
+      let content = getContent(id)
+      let title = getTitle(id)
+      let rawHTML = getRawHTML(content, title)
+      downloadHTML(filename, rawHTML)
+    },
+    generateMD: function (id, filename) {
+      let content = getContent(id)
+      let title = getTitle(id)
+      let rawHTML = getRawHTML(content, title)
+      let markdown = convertHtmlToMd(rawHTML)
+      downloadMD(filename, markdown)
+    },
+    generate: function () {
+      if (this.typeOfDev === 'Individual') {
+        this.devOrCompanyName = this.devName
+        this.iOrWe = 'I'
+        this.myOrOur = 'my'
+        this.meOrUs = 'me'
+        this.retainedInfo =
+          'retained on your device and is not collected by ' +
+          this.meOrUs +
+          ' in any way'
+      } else if (this.typeOfDev === 'Company') {
+        this.devOrCompanyName = this.companyName
+        this.iOrWe = 'we'
+        this.myOrOur = 'our'
+        this.meOrUs = 'us'
+        this.retainedInfo =
+          'retained by us and used as described in this privacy policy'
+      }
+
+      if (this.typeOfApp === 'Commercial') {
+        this.atNoCost = ''
+      } else {
+        this.atNoCost = 'at no cost'
+      }
+
+      if (this.pidInfoIn === '') {
+        this.pidInfo = '.'
+      } else {
+        this.pidInfo = ', including but not limited to ' + this.pidInfoIn + '.'
+      }
+
+      switch (this.typeOfApp) {
+        case 'Free':
+        case 'Freemium':
+        case 'Commercial':
+          this.typeOfAppTxt = 'a ' + this.typeOfApp
+          break
+        case 'Open Source':
+        case 'Ad Supported':
+          this.typeOfAppTxt = 'an ' + this.typeOfApp
+          break
+      }
+
+      switch (this.osType) {
+        case 'Android': {
+          this.osType = 'Android'
+          this.requirementOfSystem = 'system'
+          break
+        }
+        case 'iOS': {
+          this.osType = 'iOS'
+          this.requirementOfSystem = 'system'
+          break
+        }
+        case 'Android & iOS': {
+          this.osType = 'Android & iOS'
+          this.requirementOfSystem = 'both systems'
+          break
+        }
+      }
+
+      document.getElementById('privacy_tab').click()
+    }
+  }
+})
